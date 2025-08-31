eed0d95b604c6412afa4d63d5a0f53c3981c4c6d
Gary Danko <gdanko@protonmail.com>
2025-08-31 09:55:23 -0700

    Add the real wifi-status code

--- scripts/wifi-status.py
+++ scripts/wifi-status.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from pathlib import Path
 from polybar import util
 import argparse
-import subprocess
+import os
+import re
 import sys
 
 def get_memory_usage():
@@ -36,16 +38,89 @@ def get_memory_usage():
         print(f"Error running free -b -w: {e}", file=sys.stderr)
         sys.exit(1)
 
+def get_wifi_status(interfaces):
+    """
+    Execute iwconfig against each interface to get its status
+    """
+
+    statuses = []
+
+    for interface in interfaces:
+        rc, stdout, stderr = util.run_piped_command(f'iwconfig {interface}')
+        if rc == 0:
+            if stdout != '':
+                match = re.search(r"Signal level=(-?\d+)\s*dBm", stdout)
+                if match:
+                    status_dict = {
+                        'success':   True,
+                        'interface': interface,
+                        'signal':    int(match.group(1)),
+                    }
+                else:
+                    status_dict = {
+                        'success':   False,
+                        'interface': interface,
+                        'error':     f'regex failure on output',
+                    }
+            else:
+                status_dict = {
+                    'success':   False,
+                    'interface': interface,
+                    'error':     f'no output from iwconfig {interface}',
+                }
+        else:
+            if stderr != '':
+                status_dict = {
+                    'success':   False,
+                    'interface': interface,
+                    'error':     stderr.strip(),
+                }
+            else:
+                status_dict = {
+                    'success':   False,
+                    'interface': interface,
+                    'error':     f'non-zero exit code',
+                }
+        statuses.append(status_dict)
+    
+    return statuses
+
 def main():
+    config_file = os.path.join(Path.cwd(), 'wifi-status.json')
+    config, err = util.parse_config_file(config_file)
+    if err != '':
+        print(f'WiFi Status: {err}')
+        sys.exit(1)
+
     start_colorize = '%{F#F0C674}'
     end_colorize = '%{F-}'
     start_nerdfont = '%{T3}'
     end_nerdfont = '%{T-}'
-    memory_icon = util.surrogatepass('\udb80\udf5b') # md_memory
 
-    memory_info = get_memory_usage()
-    memory_usage = f'{start_colorize}{start_nerdfont}{memory_icon}{end_nerdfont}{end_colorize} {util.byte_converter(memory_info["used"])} / {util.byte_converter(memory_info["total"])}'
-    print(memory_usage)
+    parser = argparse.ArgumentParser(description="Get WiFi status from iwconfig(8)")
+    parser.add_argument("-i", "--interface", action='append', help="The interface to check; can be used multiple times", required=False)
+    args = parser.parse_args()
+
+    if args.interface:
+        interfaces = args.interface
+    else:
+        if len(config['interfaces']) == 0:
+            print('WiFi Status: No interfaces defined')
+            sys.exit(1)
+        else:
+            interfaces = config['interfaces']
+    
+    wifi_statuses = get_wifi_status(interfaces)
+    
+    output = []
+    for status in wifi_statuses:
+        if status['success']:
+            # add the icon!
+            output.append(f'{start_colorize}{status["interface"]}{end_colorize} {status["signal"]} dBm')
+        else:
+            output.append(f'{start_colorize}{status["interface"]}{end_colorize} {status["error"]}')
+    
+    print(' | '.join(output))
 
 if __name__ == "__main__":
     main()
