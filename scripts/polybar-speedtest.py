185b5ee19453553f76012125f450a258a2b31a5c
Gary Danko <gdanko@protonmail.com>
2025-09-02 18:34:59 -0700

    Need to use this name

--- scripts/polybar-speedtest.py
+++ scripts/polybar-speedtest.py
@@ -12,44 +12,83 @@ except ImportError:
     print(f'Please install speedtest-cli via pip')
     sys.exit(1)
 
-def get_formatted_speed(s,bytes=False):
-    unit = ''
-    if s > 1024**3:
-        s = s / 1024**3
-        unit = 'G'
-    elif s > 1024**2:
-        s = s / 1024**2
-        unit = 'M'
-    elif s > 1024:
-        s = s / 1024
-        unit = 'K'
-    if bytes:
-        return f'{(s/8):.2f} {unit}iB/s'
-    return f'{s:.2f} {unit}bps'
+def get_icon(speed: float=0.0) -> str:
+    # 100000000 is 100 mbit
+    if speed < 100000000:
+        return glyphs.md_speedometer_slow
+    elif speed >= 100000000 and speed < 500000000:
+        return glyphs.md_speedometer_medium
+    elif speed >= 500000000:
+        return glyphs.md_speedometer_fast
 
 def main():
-    output = []
-    s = speedtest.Speedtest()
+    speedtest_data = {
+        'down': {
+            'success': False,
+            'speed'  : 0.0,
+            'error'  : None,
+        },
+        'up': {
+            'success': False,
+            'speed'  : 0.0,
+            'error'  : None,
+        }
+    }
+
+    errors = []
+    tester = speedtest.Speedtest(secure=True)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--download', action='store_true', help='Test upload speed', required=False)
     parser.add_argument('--upload', action='store_true', help='Test upload speed', required=False)
     parser.add_argument('--bytes', action='store_true', help='Use bytes instead of bits')
-    args= parser.parse_args()
+    args = parser.parse_args()
 
     if not args.download and not args.upload:
         print(f'{util.color_title(glyphs.md_speedometer_slow)} {util.color_error("please specify --download and/or --upload")}')
         sys.exit(1)
 
     if args.download:
-        s.download()
-        output.append(f'{glyphs.cod_arrow_small_down} {get_formatted_speed(s.results.download, args.bytes)}')
+        try:
+            tester.download()
+            speedtest_data['down']['success'] = True
+            speedtest_data['down']['speed'] = tester.results.download
+        except Exception as e:
+            errors.append(e)
 
     if args.upload:
-        s.upload(pre_allocate=False)
-        output.append(f'{glyphs.cod_arrow_small_up} {get_formatted_speed(s.results.upload, args.bytes)}')
-    
-    print(f'{util.color_title(glyphs.md_speedometer_fast)} {' '.join(output)}')
+        try:
+            tester.upload(pre_allocate=False)
+            speedtest_data['up']['success'] = True
+            speedtest_data['up']['speed'] = tester.results.upload
+        except Exception as e:
+            errors.append(e)
+
+    output = []
+    if speedtest_data['down']['success']:
+        output.append(f'{glyphs.cod_arrow_small_down}{util.network_speed(tester.results.download, args.bytes)}')
+
+    if speedtest_data['up']['success']:
+        output.append(f'{glyphs.cod_arrow_small_up}{util.network_speed(tester.results.upload, args.bytes)}')
+
+    if len(output) == 0:
+         print(f'{util.color_title(glyphs.md_speedometer_fast)} {util.color_error('All tests failed')}')
+         sys.exit(1)
+
+    # Determine the icon based on average up/down speed
+    if speedtest_data['down']['success'] and speedtest_data['up']['success']:
+        icon = get_icon( (speedtest_data['down']['speed'] + speedtest_data['up']['speed']) / 2 )
+
+    elif speedtest_data['down']['success'] and not speedtest_data['up']['success']:
+        icon = get_icon( speedtest_data['down']['speed'] )
+
+    elif not speedtest_data['down']['success'] and speedtest_data['up']['success']:
+        icon = get_icon( speedtest_data['up']['speed'] )
+
+    if len(output) > 0:
+        print(f'{util.color_title(icon)} {' '.join(output)}')
+    else:
+        print(f'{util.color_title(icon)} {util.color_error('All tests failed')}')
 
 if __name__ == '__main__':
     main()
